952dbf01f738ffebd277ee61130c1b6dc7ad3046
AsimAlizade <asimelizade01@gmail.com>
2021-03-26 18:59:32 +0400

    26 March

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",component:CarComponent},
+  {path:"cars", component:CarComponent},
+  {path:"rentals", component:RentalComponent},
+  {path:"customers", component:CustomerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -10,6 +10,7 @@ import { ColorService } from 'src/app/services/color.service';
 export class ColorComponent implements OnInit {
 
   colors:Color[] = []
+  currentColor:Color;
 
   constructor(private colorService:ColorService) { }
 
@@ -23,4 +24,15 @@ export class ColorComponent implements OnInit {
     })
   }
 
+  setCurrentColor(color:Color){
+    this.currentColor = color;
+  }
+
+  getCurrentColorClass(color:Color){
+    if (color == this.currentColor) {
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
 }
